34aac83113f4b6dff869be6c9142f8a28199867b
Fred Underwood <underwoodfred@proton.me>
2024-12-23 12:34:23 +1000

    allow newly-set credential to be optionally returned

--- src/com/android/settings/password/ChooseLockPassword.java
+++ src/com/android/settings/password/ChooseLockPassword.java
@@ -193,6 +193,12 @@ public class ChooseLockPassword extends SettingsActivity {
             return this;
         }
 
+        public IntentBuilder setReturnCredentials(boolean returnCredentials) {
+            mIntent.putExtra(ChooseLockSettingsHelper.EXTRA_KEY_RETURN_CREDENTIALS,
+                    returnCredentials);
+            return this;
+        }
+
         /**
          * Configures the launch such that at the end of the password enrollment, one of its
          * managed profile (specified by {@code profileId}) will have its lockscreen unified
@@ -252,7 +258,7 @@ public class ChooseLockPassword extends SettingsActivity {
         private LockscreenCredential mChosenPassword;
         private boolean mRequestGatekeeperPassword;
         private boolean mRequestWriteRepairModePassword;
-
+        private boolean mReturnCredentials;
         private EditText mPasswordEntry;
         private TextViewInputDisabler mPasswordEntryInputDisabler;
 
@@ -657,6 +663,8 @@ public class ChooseLockPassword extends SettingsActivity {
                     ChooseLockSettingsHelper.EXTRA_KEY_REQUEST_GK_PW_HANDLE, false);
             mRequestWriteRepairModePassword = intent.getBooleanExtra(
                     ChooseLockSettingsHelper.EXTRA_KEY_REQUEST_WRITE_REPAIR_MODE_PW, false);
+            mReturnCredentials = intent.getBooleanExtra(
+                    ChooseLockSettingsHelper.EXTRA_KEY_RETURN_CREDENTIALS, false);
             if (savedInstanceState == null) {
                 updateStage(Stage.Introduction);
                 if (confirmCredentials) {
@@ -1201,7 +1209,8 @@ public class ChooseLockPassword extends SettingsActivity {
             mSaveAndFinishWorker
                     .setListener(this)
                     .setRequestGatekeeperPasswordHandle(mRequestGatekeeperPassword)
-                    .setRequestWriteRepairModePassword(mRequestWriteRepairModePassword);
+                    .setRequestWriteRepairModePassword(mRequestWriteRepairModePassword)
+                    .setReturnCredentials(mReturnCredentials);
 
             getFragmentManager().beginTransaction().add(mSaveAndFinishWorker,
                     FRAGMENT_TAG_SAVE_AND_FINISH).commit();
