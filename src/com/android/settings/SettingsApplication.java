9ea58d3fafd8a99ae38af7083fad5d29d3492e23
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-12-29 18:39:56 +0200

    add separate feature factory for Google devices

--- src/com/android/settings/SettingsApplication.java
+++ src/com/android/settings/SettingsApplication.java
@@ -42,6 +42,7 @@ import com.android.settings.metrics.SettingsMetricsLogger;
 import com.android.settings.msds.MSDLPlayerWrapper;
 import com.android.settings.overlay.FeatureFactory;
 import com.android.settings.overlay.FeatureFactoryImpl;
+import com.google.android.settings.overlay.FeatureFactoryGoogleImpl;
 import com.android.settings.spa.SettingsSpaEnvironment;
 import com.android.settingslib.applications.AppIconCacheManager;
 import com.android.settingslib.datastore.BackupRestoreStorageManager;
@@ -131,6 +132,9 @@ public class SettingsApplication extends Application {
 
     @NonNull
     protected FeatureFactory getFeatureFactory() {
+        if ("Google".equals(android.os.Build.MANUFACTURER)) {
+            return new FeatureFactoryGoogleImpl();
+        }
         return new FeatureFactoryImpl();
     }
 
