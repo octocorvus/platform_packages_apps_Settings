f77ab5aac3c7ec2a1114c1ca217e366b31f94dc9
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-01-25 13:25:51 +0200

    add per-app Play Integrity API settings

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -640,6 +640,8 @@ public class Settings extends SettingsActivity {
 
     public static class AppStorageDynCodeLoadingActivity extends SettingsActivity {}
 
+    public static class AppManagePlayIntegrityApiActivity extends SettingsActivity {}
+
     public static class ExploitProtectionActivity extends SettingsActivity {}
 
     public static class NotificationBundlesActivity extends SettingsActivity { /* empty */ }
