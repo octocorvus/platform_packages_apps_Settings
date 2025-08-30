ffb951b3f0e9940cad4642b72f9c5d281ee6f678
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-08-30 06:52:58 +0000

    BP3A: Add PIN scrambling settings for BP3A unlock settings section

--- src/com/android/settings/password/ChooseLockGeneric.java
+++ src/com/android/settings/password/ChooseLockGeneric.java
@@ -98,6 +98,7 @@ import com.android.settings.security.screenlock.AutoPinConfirmPreferenceControll
 import com.android.settings.security.screenlock.LockAfterTimeoutPreferenceController;
 import com.android.settings.security.screenlock.PatternVisiblePreferenceController;
 import com.android.settings.security.screenlock.PinPrivacyPreferenceController;
+import com.android.settings.security.screenlock.PinScramblingPrefController;
 import com.android.settings.security.screenlock.PowerButtonInstantLockPreferenceController;
 import com.android.settingslib.RestrictedPreference;
 import com.android.settingslib.core.AbstractPreferenceController;
@@ -723,6 +724,7 @@ public class ChooseLockGeneric extends SettingsActivity {
             mAutoPinConfirmPreferenceController = new AutoPinConfirmPreferenceController(
                     getContext(), mUserId, mLockPatternUtils, this);
             mUnlockSettingsControllers.add(mAutoPinConfirmPreferenceController);
+            mUnlockSettingsControllers.add(new PinScramblingPrefController(requireContext(), Primary));
         }
 
         private void setUpUnlockSettingsPreference() {
