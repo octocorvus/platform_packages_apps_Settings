a0a6f2abe2c0f85edf2ada1c9bf907f50d30ad61
octocorvus <admin@octocorvus.dev>
2025-09-26 10:20:18 +0000

    fixup! RCS support

--- src/com/android/settings/applications/specialaccess/GoogleSpecialDeviceIdentifiersAccessFragment.java
+++ src/com/android/settings/applications/specialaccess/GoogleSpecialDeviceIdentifiersAccessFragment.java
@@ -3,9 +3,12 @@ package com.android.settings.applications.specialaccess;
 import android.ext.settings.BoolSetting;
 import android.ext.settings.ExtSettings;
 
+import com.android.settings.R;
 import com.android.settings.ext.BoolSettingFragment;
+import com.android.settingslib.widget.FooterPreference;
 
 public class GoogleSpecialDeviceIdentifiersAccessFragment extends BoolSettingFragment {
+
     @Override
     protected BoolSetting getSetting() {
         return ExtSettings.ALLOW_GOOGLE_APPS_SPECIAL_ACCESS_TO_DEVICE_IDENTIFIERS;
@@ -13,6 +16,17 @@ public class GoogleSpecialDeviceIdentifiersAccessFragment extends BoolSettingFra
 
     @Override
     protected CharSequence getTitle() {
-        return "(TODO) " + GoogleSpecialDeviceIdentifiersAccessPrefController.class.getSimpleName();
+        return resText(R.string.Google_apps_special_device_identifiers_access_title);
+    }
+
+    @Override
+    protected CharSequence getMainSwitchTitle() {
+        return resText(R.string.Google_apps_special_device_identifiers_access_main_switch);
+    }
+
+    @Override
+    protected FooterPreference makeFooterPref(FooterPreference.Builder builder) {
+        return builder.setTitle(R.string.Google_apps_special_device_identifiers_access_footer)
+                .build();
     }
 }
--- src/com/android/settings/applications/specialaccess/GoogleSpecialDeviceIdentifiersAccessPrefController.java
+++ src/com/android/settings/applications/specialaccess/GoogleSpecialDeviceIdentifiersAccessPrefController.java
@@ -3,48 +3,23 @@ package com.android.settings.applications.specialaccess;
 import android.content.Context;
 import android.ext.settings.ExtSettings;
 
-import com.android.settings.ext.BoolSettingPrefController;
+import com.android.settings.R;
+import com.android.settings.ext.BoolSettingFragmentPrefController;
 
-public class GoogleSpecialDeviceIdentifiersAccessPrefController extends BoolSettingPrefController {
-
-    private static final String TAG =
-            GoogleSpecialDeviceIdentifiersAccessPrefController.class.getSimpleName();
+public class GoogleSpecialDeviceIdentifiersAccessPrefController extends
+        BoolSettingFragmentPrefController {
 
     public GoogleSpecialDeviceIdentifiersAccessPrefController(Context ctx, String key) {
         super(ctx, key, ExtSettings.ALLOW_GOOGLE_APPS_SPECIAL_ACCESS_TO_DEVICE_IDENTIFIERS);
     }
 
-//    @Override
-//    protected void getEntries(Entries entries) {
-//        entries.add("USER_NULL", UserHandle.USER_NULL);
-//
-//        var um = mContext.getSystemService(UserManager.class);
-//
-//        addUserAndProfilesToEntry(um.getUserInfo(um.getMainUser().getIdentifier()), um, entries);
-//
-//        for (UserInfo ui : um.getAliveUsers()) {
-//            if (ui.isMain()) continue;
-//            addUserAndProfilesToEntry(ui, um, entries);
-//        }
-//    }
-//
-//    private void addUserAndProfilesToEntry(UserInfo ui, UserManager um, Entries entries) {
-//        Log.d(TAG, "user: " + ui.toFullString());
-//        if (!ui.isFull() || !ui.isEnabled() || ui.isGuest()) {
-//            return;
-//        }
-//        entries.add(ui.name, ui.id);
-//        for (UserInfo subUi : um.getProfiles(ui.id)) {
-//            Log.d(TAG, "sub user: " + ui.toFullString());
-//            if (subUi.id == ui.id || !subUi.isEnabled()) {
-//                continue;
-//            }
-//            entries.add(subUi.name + " (" + ui.name + ")", subUi.id);
-//        }
-//    }
+    @Override
+    protected CharSequence getSummaryOn() {
+        return resText(R.string.Google_apps_special_device_identifiers_access_summary_granted);
+    }
 
-//    @Override
-//    protected boolean isCredentialConfirmationRequired() {
-//        return true;
-//    }
+    @Override
+    protected CharSequence getSummaryOff() {
+        return resText(R.string.Google_apps_special_device_identifiers_access_summary_not_granted);
+    }
 }
