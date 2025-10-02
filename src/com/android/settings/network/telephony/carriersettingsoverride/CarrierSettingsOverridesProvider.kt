cb2ae8be54a8aa347cd9a15aa6349d83edcc6131
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-10-03 00:08:53 +0300

    adjust carrier config overrides UI

--- src/com/android/settings/network/telephony/carriersettingsoverride/CarrierSettingsOverridesProvider.kt
+++ src/com/android/settings/network/telephony/carriersettingsoverride/CarrierSettingsOverridesProvider.kt
@@ -46,6 +46,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavType
 import androidx.navigation.navArgument
 import com.android.settings.R
+import com.android.settings.network.telephony.carriersettingsoverride.CarrierSettingsOverridesViewModel.MessageType
 import com.android.settings.spa.network.CollectAirplaneModeAndFinishIfOn
 import com.android.settingslib.spa.framework.common.SettingsPageProvider
 import com.android.settingslib.spa.framework.theme.SettingsDimension
@@ -64,7 +65,6 @@ import com.android.settingslib.spa.widget.ui.SettingsIcon
 import com.android.settingslib.spaprivileged.model.enterprise.Restrictions
 import com.android.settingslib.spaprivileged.template.preference.RestrictedMainSwitchPreference
 import com.android.settingslib.spaprivileged.template.preference.RestrictedPreference
-import com.android.settings.network.telephony.carriersettingsoverride.CarrierSettingsOverridesViewModel.MessageType
 
 private const val SUB_ID_FOR_OVERRIDE = "subId"
 
