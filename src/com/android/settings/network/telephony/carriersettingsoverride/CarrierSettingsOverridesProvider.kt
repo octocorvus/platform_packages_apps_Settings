377be93cf8633af93649fda48ede53fc52baeed9
inthewaves <inthewaves@pm.me>
2025-10-07 13:32:31 -0700

    support summary in carrier config override dialog buttons

--- src/com/android/settings/network/telephony/carriersettingsoverride/CarrierSettingsOverridesProvider.kt
+++ src/com/android/settings/network/telephony/carriersettingsoverride/CarrierSettingsOverridesProvider.kt
@@ -267,6 +267,9 @@ private fun CarrierSettingOverrideOptionPreference(
                     if (!possibleState.isUserSelectable) return@forEachIndexed
                     Radio(
                         text = getSelectionText(context, flagState, possibleState),
+                        summary = (possibleState as? ConfigState.ActiveState)
+                            ?.summaryStringRes
+                            ?.let(context::getString),
                         selected = index == selectedIndexForThisFlag,
                         enabled = !isOverrideInProgress && !isOverrideActive,
                         onSelected = {
